663df3d3545a8956b9c77808a7cb538e0896b054
adriencohade <48437936+adriencohade@users.noreply.github.com>
2019-03-20 11:26:39 +0100

    proj

---
+++ proj/src/main.ts
@@ -0,0 +1,11 @@
+﻿import { sayHello } from "./greet";
+import { example } from "./example";
+
+function showHello(divName: string, name: string) {
+    const elt = document.getElementById(divName);
+    elt.innerText = sayHello(name);
+}
+
+showHello("greeting", "TypeScript");
+
+new example();
